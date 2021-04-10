f784deb0bae95ce4ce6ff92a545fe667cf2450d8
aljt <alvinljt@gmail.com>
2021-04-11 01:49:12 +0800

    Added comments and seed data for rides table.

--- src/schemas.js
+++ src/schemas.js
@@ -16,7 +16,36 @@ module.exports = (db) => {
         )
     `;
 
+    // create the Rides table
     db.run(createRideTableSchema);
+
+    for (let i = 1; i < 11; i += 1) {
+        const insertQuery = 'INSERT INTO Rides (startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES($startLat, $startLong, $endLat, $endLong, $riderName, $driverName, $driverVehicle)';
+
+        const values = {
+            $startLat: -70+i,
+            $startLong: 90+i,
+            $endLat: -75+i,
+            $endLong: 95+i,
+            $riderName: `rider${i}`,
+            $driverName: `driver${i}`,
+            $driverVehicle: "Toyota Prius"
+        };
+
+        // seed the ride into the database
+        db.run(insertQuery, values);
+    }
+
+    // // Seed rides into the database
+    // db.run("INSERT INTO Rides (startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES($startLat, $startLong, $endLat, $endLong, $riderName, $driverName, $driverVehicle)", {
+    //     $startLat: -80,
+    //     $startLong: 90,
+    //     $endLat: -75,
+    //     $endLong: 95,
+    //     $riderName: "rider1",
+    //     $driverName: "driver1",
+    //     $driverVehicle: "Toyota Prius"
+    // });
 
     return db;
 };
