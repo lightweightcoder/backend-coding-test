175752f8126143dc64d96ec4bb98e166d9f1a762
aljt <alvinljt@gmail.com>
2021-04-12 22:41:41 +0800

    Implemented async await for sqlite3 api callbacks.
    Integrated sqlite package to enable promises for sqlite3. Implemented promise-based async await for database queries. Refactored database queries into functions in rides controller.

--- src/app.js
+++ src/app.js
@@ -7,17 +7,19 @@ const bodyParser = require('body-parser');
 
 const jsonParser = bodyParser.json();
 
-const store = require('./store');
-
-// Get the number of rides per page
-const { RIDES_PER_PAGE } = store;
+// Get the function to initalise the rides controller.
+// The rides controller performs database queries.
+const initRidesController = require('../controllers/rides.js');
 
 module.exports = (db) => {
+  // initialise the controllers
+  const ridesController = initRidesController(db);
+
   // Route to test if the server is running.
   app.get('/health', (req, res) => res.send('Healthy'));
 
   // Route to add a ride into the rides table of the database.
-  app.post('/rides', jsonParser, (req, res) => {
+  app.post('/rides', jsonParser, async (req, res) => {
     // Get the ride details from the request body
     const startLatitude = Number(req.body.start_lat);
     const startLongitude = Number(req.body.start_long);
@@ -75,87 +77,53 @@ module.exports = (db) => {
 
     const values = [req.body.start_lat, req.body.start_long, req.body.end_lat, req.body.end_long, req.body.rider_name, req.body.driver_name, req.body.driver_vehicle];
 
-    // Attempt to insert a new ride into the rides table.
-    // Using values (2nd argument) automatically sanitises the values.
-    db.run('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7)', values, function (err) {
-      // If an error occured, sent an error response.
-      if (err) {
-        return res.send({
-          error_code: 'SERVER_ERROR',
-          message: 'Unknown error',
-        });
-      }
+    // Add a new ride to rides table
+    const addNewRideResult = await ridesController.addNewRide(values);
 
-      // Query the rides tables for the newly created ride.
-      db.all('SELECT * FROM Rides WHERE rideID = ?', this.lastID, (selectErr, rows) => {
-        if (selectErr) {
-          return res.send({
-            error_code: 'SERVER_ERROR',
-            message: 'Unknown error',
-          });
-        }
+    // If there is an error, send it to the response
+    if (addNewRideResult.error_code) {
+      return res.send(addNewRideResult);
+    }
 
-        // Send the rides data to the response.
-        res.send(rows);
-      });
-    });
+    // If there is no error, get the rideID of the new ride
+    const newRideId = addNewRideResult.lastID;
+
+    // Retrieve the ride's data
+    const retrieveRideResult = await ridesController.retrieveRide(newRideId);
+
+    // Return the retrieved result.
+    return res.send(retrieveRideResult);
   });
 
-  // Route to retrieve all rides from the database.
-  app.get('/rides', (req, res) => {
+  // Route to retrieve all rides of a single page from the database.
+  app.get('/rides', async (req, res) => {
     // Get the page number from query parameter.
+    // If req.query.page is not a number, page == NaN
+    // If req.query.page is an empty string or null, page == 0
     const page = Number(req.query.page);
 
-    // Calculate which rideID to begin retrieving rides.
-    const startingRideId = (page - 1) * RIDES_PER_PAGE + 1;
+    // If page is not a number, return a validation error message
+    if (Number.isNaN(page) || page === 0) {
+      return res.send({
+        error_code: 'VALIDATION_ERROR',
+        message: 'Page must be an integer',
+      });
+    }
 
-    const values = [startingRideId, RIDES_PER_PAGE];
+    // Retrieve the rides data for that page.
+    const retrieveRidesResult = await ridesController.retrieveRides(page);
 
-    // Query the rides table for rides for that page.
-    db.all('SELECT * FROM Rides WHERE rideID >= ?1 ORDER BY rideID LIMIT ?2', values, (err, rows) => {
-      // If an error occured, sent an error response.
-      if (err) {
-        return res.send({
-          error_code: 'SERVER_ERROR',
-          message: 'Unknown error',
-        });
-      }
-
-      // If there are no rides, send an error response.
-      if (rows.length === 0) {
-        return res.send({
-          error_code: 'RIDES_NOT_FOUND_ERROR',
-          message: 'Could not find any rides',
-        });
-      }
-
-      // Send the rides data to the response.
-      res.send(rows);
-    });
+    // Return the retrieved result.
+    return res.send(retrieveRidesResult);
   });
 
   // Route to retrieve a ride from the database.
-  app.get('/rides/:id', (req, res) => {
-    db.all(`SELECT * FROM Rides WHERE rideID='${req.params.id}'`, (err, rows) => {
-      // If an error occured, sent an error response.
-      if (err) {
-        return res.send({
-          error_code: 'SERVER_ERROR',
-          message: 'Unknown error',
-        });
-      }
+  app.get('/rides/:id', async (req, res) => {
+    // Retrieve the ride's data
+    const retrieveRideResult = await ridesController.retrieveRide(req.params.id);
 
-      // If there are no rides of coressponding to the selected rideId, send an error response.
-      if (rows.length === 0) {
-        return res.send({
-          error_code: 'RIDES_NOT_FOUND_ERROR',
-          message: 'Could not find any rides',
-        });
-      }
-
-      // Send the ride data to the response.
-      res.send(rows);
-    });
+    // Return the retrieved result.
+    return res.send(retrieveRideResult);
   });
 
   return app;
