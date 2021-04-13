a5cedc0c1397202e8fbce5e598031a71c7011a88
aljt <alvinljt@gmail.com>
2021-04-13 15:27:35 +0800

    Removed commented out code.

--- index.js
+++ index.js
@@ -25,13 +25,3 @@ open({
     app.listen(port, () => console.log(`App started and listening on port ${port}`));
   });
 });
-
-// // Serialize ensures that only 1 database query can be executed at a time to prevent data corruption.
-// db.serialize(() => {
-//   buildSchemas(db);
-
-//   // eslint-disable-next-line global-require
-//   const app = require('./src/app')(db);
-
-//   app.listen(port, () => console.log(`App started and listening on port ${port}`));
-// });
